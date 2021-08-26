ec451b8f1d52334cb34511bbc089a610033e0986
Sven Lange <svenlange@mailbox.org>
2021-08-26 12:55:32 +0200

    add function get_camera_viewport_rect even it is not needed by now.

--- Camera.gd
+++ Camera.gd
@@ -16,10 +16,10 @@ is corresponding to this mechanic.
 func _ready():
 	# center the camera to the cursor.
 	offset = Global.cell_size / 2
-
 	# TODO: drag margins need to be adjusted to the size of the cursor.
-	# But only, if dragging is used sometimes.
+	# But only, if dragging is used sometime in the future.
 	
+
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.pressed:
@@ -47,3 +47,13 @@ func _process(_delta):
 #		offset = Vector2.ZERO
 ##		force_update_scroll()
 ##		align()
+
+func get_camera_viewport_rect() -> Rect2:
+	"""
+	returns the actual visible rect of the camera including the zoom.
+	"""
+	var cameraPos = get_camera_screen_center()
+	var viewportRect = get_viewport_rect().size * zoom
+	var pos_x = cameraPos.x - viewportRect.x / 2
+	var pos_y = cameraPos.y - viewportRect.y / 2
+	return Rect2(Vector2(pos_x, pos_y), viewportRect)
