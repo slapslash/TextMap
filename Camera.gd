8b4cbcc7ea9c1dd72e5c601c2923d68b25c7d977
Sven Lange <svenlange@mailbox.org>
2021-08-27 11:20:27 +0200

    optimize the inital centering of the screen and adapt it to the window-stretch mode is now disabled.

--- Camera.gd
+++ Camera.gd
@@ -18,19 +18,16 @@ is corresponding to this mechanic.
 func _ready():
 	# TODO: drag margins need to be adjusted to the size of the cursor.
 	# But only, if dragging is used sometime in the future.
-	_initial_zoom_and_positioning()
+	_initial_positioning()
 	emit_signal("zoom_changed", zoom.x)
 
-func _initial_zoom_and_positioning():
+
+func _initial_positioning():
 	"""
-	zoom out a litte bit, that a whole screen is visible and
-	position the camera, so that this screen is centered.
+	position the camera, so that the first screen is centered.
 	"""
-	var rect = get_camera_viewport_rect()
-	zoom = Vector2(1.1, 1.1)
-	var zoomed_rect = get_camera_viewport_rect()
-	var diff = zoomed_rect.size - rect.size
-	offset = -diff / 2
+	offset = (Global.screen_size_pixels - get_viewport_rect().size) * 0.5
+
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -61,13 +58,3 @@ func _process(_delta):
 #		offset = Vector2.ZERO
 ##		force_update_scroll()
 ##		align()
-
-func get_camera_viewport_rect() -> Rect2:
-	"""
-	returns the actual visible rect of the camera including the zoom.
-	"""
-	var cameraPos = get_camera_screen_center()
-	var viewportRect = Global.screen_size_pixels * zoom
-	var pos_x = cameraPos.x - viewportRect.x / 2
-	var pos_y = cameraPos.y - viewportRect.y / 2
-	return Rect2(Vector2(pos_x, pos_y), viewportRect)
