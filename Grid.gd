c120bc74785922d8f955c7bb46dc6f56ca6f9048
Sven Lange <svenlange@mailbox.org>
2021-09-02 09:15:03 +0200

    only show grid, if activated in global settings.

--- Grid.gd
+++ Grid.gd
@@ -1,18 +1,19 @@
 extends Node2D
 
-onready var n_screens_around = 10
+onready var n_screens_around = 30
 
 
 func _draw():
-	var s = Global.screen_size_pixels
-	var start: Vector2
-	var end: Vector2
-	for i in range(-n_screens_around, (n_screens_around + 2)):
-		start = Vector2(i * s.x, -n_screens_around * s.y)
-		end = Vector2(i * s.x, (n_screens_around + 1) * s.y)
-		draw_line(start, end, Color.dimgray, 1.0, true)
-		
-		start = Vector2(-n_screens_around * s.x, i * s.y)
-		end = Vector2((n_screens_around + 1) * s.x, i * s.y)
-		draw_line(start, end, Color.dimgray, 1.0, true)
+	if Global.show_grid:
+		var s = Global.screen_size_pixels
+		var start: Vector2
+		var end: Vector2
+		for i in range(-n_screens_around, (n_screens_around + 2)):
+			start = Vector2(i * s.x, -n_screens_around * s.y)
+			end = Vector2(i * s.x, (n_screens_around + 1) * s.y)
+			draw_line(start, end, Color.dimgray, 1.0, true)
+			
+			start = Vector2(-n_screens_around * s.x, i * s.y)
+			end = Vector2((n_screens_around + 1) * s.x, i * s.y)
+			draw_line(start, end, Color.dimgray, 1.0, true)
 
