b74b73e20ab9f02d05ac4c00e3d4e047f1ac40b3
Sven Lange <svenlange@mailbox.org>
2021-09-29 10:29:23 +0200

    Add a little neat walking animation and adjust the collision a little bit.

--- Player.gd
+++ Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+onready var anim = $AnimationPlayer
+
 export (int) var speed = 100
 
 var velocity = Vector2()
@@ -14,7 +16,7 @@ func _ready():
 	VisualServer.set_default_clear_color(Color.black)
 
 func get_input():
-	velocity = Vector2()
+	velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
@@ -24,6 +26,12 @@ func get_input():
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
+
+	if velocity != Vector2.ZERO:
+		anim.play("Walk")
+	elif anim.is_playing():
+		anim.seek(0, true)
+		anim.stop()
 
 func _physics_process(_delta):
 	get_input()
